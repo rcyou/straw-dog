2824afcab8e258670a97c2a03f2cf884bfa2f207
Ryan Young <ryoung.hello@gmail.com>
2026-06-06 08:26:52 -0500

    Fix project structure, merge tsconfig, and verify local build

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
 // https://astro.build/config
 export default defineConfig({
-  output: 'hybrid',
+  output: 'static',
   adapter: cloudflare()
 });
